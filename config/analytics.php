5050bc4c2f17b07cc391f6083b6d344b3fccfe76
Wilhelm Altemeier <55293156+Wiiii90@users.noreply.github.com>
2026-08-18 06:12:46 +0200

    Separate Matomo tracking from reporting

--- config/analytics.php
+++ config/analytics.php
@@ -2,7 +2,8 @@
 
 return [
     'matomo' => [
-        'enabled' => (bool) env('MATOMO_ENABLED', false),
+        'tracking_enabled' => (bool) env('MATOMO_TRACKING_ENABLED', env('MATOMO_ENABLED', false)),
+        'reporting_enabled' => (bool) env('MATOMO_REPORTING_ENABLED', env('MATOMO_ENABLED', false)),
         'base_url' => env('MATOMO_BASE_URL'),
         'site_id' => env('MATOMO_SITE_ID'),
         'api_token' => env('MATOMO_API_TOKEN'),
