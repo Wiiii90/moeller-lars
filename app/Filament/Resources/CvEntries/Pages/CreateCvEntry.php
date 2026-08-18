b44db8fad47a30555915df45259b1ef62dcb7f0e
Wilhelm Altemeier <55293156+Wiiii90@users.noreply.github.com>
2026-08-18 05:05:28 +0200

    Validate Vita rich text before creation

--- app/Filament/Resources/CvEntries/Pages/CreateCvEntry.php
+++ app/Filament/Resources/CvEntries/Pages/CreateCvEntry.php
@@ -3,6 +3,7 @@
 namespace App\Filament\Resources\CvEntries\Pages;
 
 use App\Domain\Admin\AdminAuditService;
+use App\Domain\Admin\EditorialRichTextValidator;
 use App\Filament\Resources\CvEntries\CvEntryResource;
 use App\Models\CvEntry;
 use Filament\Resources\Pages\CreateRecord;
@@ -15,6 +16,8 @@ class CreateCvEntry extends CreateRecord
 
     protected function mutateFormDataBeforeCreate(array $data): array
     {
+        app(EditorialRichTextValidator::class)->validate($data['body'] ?? null, 'body');
+
         foreach (['state', 'position', 'published_at', 'legacy_id', 'legacy_source', 'migration_batch_id', 'migrated_at'] as $field) {
             unset($data[$field]);
         }
