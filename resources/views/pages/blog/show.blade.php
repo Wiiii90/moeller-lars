4fdd1375ccd3249a3e13c19dbbab0f4b74fccf2b
Wilhelm Altemeier <55293156+Wiiii90@users.noreply.github.com>
2026-08-18 06:07:31 +0200

    Track blog reads in Matomo

--- resources/views/pages/blog/show.blade.php
+++ resources/views/pages/blog/show.blade.php
@@ -5,7 +5,12 @@
 @section('canonical', app(\App\Domain\Content\CanonicalUrl::class)->forPath('/blog/'.$post->slug))
 
 @section('content')
-    <article class="blog-post">
+    <article
+        class="blog-post"
+        data-matomo-event-category="Blog"
+        data-matomo-event-on-load="blog_view"
+        data-matomo-event-name="{{ $post->title }}"
+    >
         <h2 class="category-heading">{{ $post->title }}</h2>
         @if ($post->coverMedia !== null)
             <img class="blog-post__cover" src="{{ $media->thumbnailUrlForAsset($post->coverMedia) }}" alt="{{ $media->altTextForAsset($post->coverMedia) }}">
