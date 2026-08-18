2632090896e2ba86a65a48f542cb8db00f87cda6
Wilhelm Altemeier <55293156+Wiiii90@users.noreply.github.com>
2026-08-19 00:32:45 +0200

    Configure operator media allowance

--- config/media.php
+++ config/media.php
@@ -1,5 +1,10 @@
 <?php
 
+$quotaBytes = env('MEDIA_STORAGE_QUOTA_BYTES');
+
 return [
     'disk' => env('MEDIA_DISK', 'local'),
+    'quota_bytes' => is_string($quotaBytes) && ctype_digit($quotaBytes) && (int) $quotaBytes > 0
+        ? (int) $quotaBytes
+        : null,
 ];
