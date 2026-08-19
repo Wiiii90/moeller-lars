62b4e70289f83cda7a4542ce65eb98d5bd1c4cb9
Wilhelm Altemeier <55293156+Wiiii90@users.noreply.github.com>
2026-08-19 20:40:19 +0200

    Relate artwork categories to canonical Gallery sections

--- app/Models/ArtworkCategory.php
+++ app/Models/ArtworkCategory.php
@@ -8,6 +8,7 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\Relations\HasOne;
 
 #[Fillable(['parent_id', 'slug', 'name', 'state', 'position', 'show_in_navigation', 'show_on_home', 'description', 'legacy_id', 'legacy_source', 'migration_batch_id', 'migrated_at'])]
 #[Guarded(['id'])]
@@ -33,6 +34,11 @@ class ArtworkCategory extends Model
     public function children(): HasMany
     {
         return $this->hasMany(self::class, 'parent_id');
+    }
+
+    public function siteSection(): HasOne
+    {
+        return $this->hasOne(SiteSection::class, 'artwork_category_id');
     }
 
     public function artworks(): HasMany
