33c3d12dd2d1094ec181540f169bdc8ee1112231
Wilhelm Altemeier <55293156+Wiiii90@users.noreply.github.com>
2026-08-20 22:32:38 +0200

    Keep blog settings explicit

--- app/Models/BlogSetting.php
+++ app/Models/BlogSetting.php
@@ -2,18 +2,24 @@
 
 namespace App\Models;
 
-use App\Models\Concerns\SingletonRecord;
 use Illuminate\Database\Eloquent\Attributes\Fillable;
 use Illuminate\Database\Eloquent\Attributes\Guarded;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use LogicException;
 
 #[Fillable(['listing_title', 'listing_intro'])]
 #[Guarded(['id'])]
 class BlogSetting extends Model
 {
     use HasFactory;
-    use SingletonRecord;
 
     protected $table = 'blog_settings';
+
+    protected static function booted(): void
+    {
+        static::deleting(function (): never {
+            throw new LogicException('Blog settings cannot be deleted.');
+        });
+    }
 }
