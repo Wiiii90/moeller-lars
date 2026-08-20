728401f3e750ccb56d6ae033435efbe050a0c855
Wilhelm Altemeier <55293156+Wiiii90@users.noreply.github.com>
2026-08-20 22:34:26 +0200

    Use explicit settings record lookup

--- app/Filament/Resources/PublicContentSettings/PublicContentSettingResource.php
+++ app/Filament/Resources/PublicContentSettings/PublicContentSettingResource.php
@@ -36,7 +36,7 @@ class PublicContentSettingResource extends Resource
 
     public static function getNavigationUrl(): string
     {
-        return static::getUrl('edit', ['record' => PublicContentSetting::current()]);
+        return static::getUrl('edit', ['record' => PublicContentSetting::query()->sole()]);
     }
 
     public static function form(Schema $schema): Schema
