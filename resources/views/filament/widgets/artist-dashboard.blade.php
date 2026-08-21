b8b02ac0b42d17a6e73d7a3a49f6052895138aa5
Wilhelm Altemeier <55293156+Wiiii90@users.noreply.github.com>
2026-08-21 04:29:42 +0200

    Restore adaptive quick action context

--- resources/views/filament/widgets/artist-dashboard.blade.php
+++ resources/views/filament/widgets/artist-dashboard.blade.php
@@ -14,7 +14,7 @@
 
         @if ($quickActions !== [])
             <nav class="artist-dashboard__quick-actions" aria-label="Personalized quick actions">
-                <span>For you</span>
+                <span>For you · Based on repeated admin work</span>
                 @foreach ($quickActions as $action)
                     <a href="{{ $action['url'] }}" title="{{ $action['reason'] }}">{{ $action['label'] }}</a>
                 @endforeach
