fa9f21a8d29484ca6cd2de3bd92e9172584c0fc8
Wilhelm Altemeier <55293156+Wiiii90@users.noreply.github.com>
2026-08-21 10:24:48 +0200

    Show sign out as explicit topbar action

--- resources/views/filament/partials/topbar-sign-out.blade.php
+++ resources/views/filament/partials/topbar-sign-out.blade.php
@@ -1,11 +1,8 @@
 <form class="artist-topbar-sign-out" method="POST" action="{{ filament()->getLogoutUrl() }}">
     @csrf
 
-    <x-filament::icon-button
-        color="gray"
-        icon="heroicon-m-arrow-left-on-rectangle"
-        icon-alias="panels::user-menu.logout-button"
-        label="Sign out"
-        type="submit"
-    />
+    <button class="artist-topbar-sign-out__button" type="submit" aria-label="Sign out">
+        <x-filament::icon icon="heroicon-m-arrow-left-on-rectangle" class="artist-topbar-sign-out__icon" />
+        <span>Sign out</span>
+    </button>
 </form>
