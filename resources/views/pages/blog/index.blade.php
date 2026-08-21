cdc236e2ed5f5cf763cb31e0f99859c5f50fe09f
Wilhelm Altemeier <55293156+Wiiii90@users.noreply.github.com>
2026-08-21 04:59:25 +0200

    Fix preview blog Blade compilation

--- resources/views/pages/blog/index.blade.php
+++ resources/views/pages/blog/index.blade.php
@@ -5,7 +5,9 @@
 @section('canonical', app(\App\Domain\Content\CanonicalUrl::class)->forPath('/blog'))
 
 @section('content')
-    @php($preview = app(\App\Domain\Content\SitePreviewContext::class))
+    @php
+        $preview = app(\App\Domain\Content\SitePreviewContext::class);
+    @endphp
     <section class="blog-page" aria-labelledby="blog-heading">
         <h2 id="blog-heading" class="category-heading">{{ $settings->listing_title ?: 'Blog' }}</h2>
 
@@ -14,7 +16,9 @@
         @endif
 
         @foreach ($posts as $post)
-            @php($postUrl = $preview->url(route('blog.show', ['slug' => $post->slug])))
+            @php
+                $postUrl = $preview->url(route('blog.show', ['slug' => $post->slug]));
+            @endphp
             <article class="blog-entry">
                 <h3><a href="{{ $postUrl }}">{{ $post->title }}</a></h3>
                 @if ($post->coverMedia !== null)
