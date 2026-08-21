931d39be9b28efab68d1ea177d3f484cc849e032
Wilhelm Altemeier <55293156+Wiiii90@users.noreply.github.com>
2026-08-22 00:43:35 +0200

    Use shared editor overlay for legacy Vita settings

--- app/Filament/Resources/VitaContentSettings/Pages/EditVitaContentSetting.php
+++ app/Filament/Resources/VitaContentSettings/Pages/EditVitaContentSetting.php
@@ -3,6 +3,8 @@
 namespace App\Filament\Resources\VitaContentSettings\Pages;
 
 use App\Domain\Admin\AdminSettingsService;
+use App\Filament\Concerns\UsesEditorOverlay;
+use App\Filament\Pages\SitePages;
 use App\Filament\Resources\VitaContentSettings\VitaContentSettingResource;
 use App\Models\PublicContentSetting;
 use Filament\Resources\Pages\EditRecord;
@@ -10,16 +12,23 @@ use Illuminate\Database\Eloquent\Model;
 
 final class EditVitaContentSetting extends EditRecord
 {
+    use UsesEditorOverlay;
+
     protected static string $resource = VitaContentSettingResource::class;
 
     public function getBreadcrumbs(): array
     {
-        return ['Pages', 'Vita'];
+        return [];
     }
 
     protected function handleRecordUpdate(Model $record, array $data): Model
     {
         /** @var PublicContentSetting $record */
         return app(AdminSettingsService::class)->updatePublicContent($record, $data);
+    }
+
+    protected function getRedirectUrl(): string
+    {
+        return $this->editorReturnUrl(SitePages::getUrl());
     }
 }
