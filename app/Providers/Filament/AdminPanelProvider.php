97f4f9c0b95893a605d078290cbc36448066afda
Wilhelm Altemeier <55293156+Wiiii90@users.noreply.github.com>
2026-08-21 07:51:07 +0200

    Run Matomo deferral across admin Livewire requests

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers\Filament;
 
 use App\Filament\Widgets\ArtistDashboard;
+use App\Http\Middleware\DeferMatomoReporting;
 use Filament\Http\Middleware\Authenticate;
 use Filament\Http\Middleware\AuthenticateSession;
 use Filament\Http\Middleware\DisableBladeIconComponents;
@@ -60,6 +61,7 @@ class AdminPanelProvider extends PanelProvider
             ])
             ->authMiddleware([
                 Authenticate::class,
-            ]);
+                DeferMatomoReporting::class,
+            ], isPersistent: true);
     }
 }
