c07628a4e8cc47e2061f1617b25dae3278d4b3c4
Wilhelm Altemeier <55293156+Wiiii90@users.noreply.github.com>
2026-08-21 08:31:42 +0200

    Remove redundant dashboard page heading

--- app/Filament/Pages/Dashboard.php
+++ app/Filament/Pages/Dashboard.php
@@ -4,12 +4,18 @@ namespace App\Filament\Pages;
 
 use App\Filament\Widgets\ArtistDashboard;
 use Filament\Pages\Dashboard as BaseDashboard;
+use Illuminate\Contracts\Support\Htmlable;
 
 final class Dashboard extends BaseDashboard
 {
     protected static ?string $title = 'Dashboard';
 
     protected static ?int $navigationSort = -2;
+
+    public function getHeading(): string|Htmlable|null
+    {
+        return null;
+    }
 
     public function getColumns(): array
     {
