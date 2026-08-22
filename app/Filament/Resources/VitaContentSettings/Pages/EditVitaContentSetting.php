b56c229a88f0c4ef2470bcbdfdfa847296f61ac5
Wilhelm Altemeier <55293156+Wiiii90@users.noreply.github.com>
2026-08-22 14:49:12 +0200

    Move Vita settings onto route editor contract

--- app/Filament/Resources/VitaContentSettings/Pages/EditVitaContentSetting.php
+++ app/Filament/Resources/VitaContentSettings/Pages/EditVitaContentSetting.php
@@ -3,7 +3,7 @@
 namespace App\Filament\Resources\VitaContentSettings\Pages;
 
 use App\Domain\Admin\AdminSettingsService;
-use App\Filament\Concerns\UsesEditorOverlay;
+use App\Filament\Concerns\UsesAdminEditor;
 use App\Filament\Pages\SitePages;
 use App\Filament\Resources\VitaContentSettings\VitaContentSettingResource;
 use App\Models\PublicContentSetting;
@@ -12,7 +12,7 @@ use Illuminate\Database\Eloquent\Model;
 
 final class EditVitaContentSetting extends EditRecord
 {
-    use UsesEditorOverlay;
+    use UsesAdminEditor;
 
     protected static string $resource = VitaContentSettingResource::class;
 
