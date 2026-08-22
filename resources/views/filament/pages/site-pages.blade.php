e18f57b428f009f6fe27ae96a6f79809cc01491e
Wilhelm Altemeier <55293156+Wiiii90@users.noreply.github.com>
2026-08-22 02:16:17 +0200

    Render Pages as a typed node tree

--- resources/views/filament/pages/site-pages.blade.php
+++ resources/views/filament/pages/site-pages.blade.php
@@ -7,13 +7,19 @@
             </div>
         </header>
 
-        <section class="artist-page-list" aria-label="Public site structure">
+        <section class="artist-page-list artist-page-tree" aria-label="Public site structure">
             @foreach ($sections as $section)
-                @include('filament.pages.partials.site-section-row', ['section' => $section])
+                <div class="artist-page-tree__branch" data-has-children="{{ $section['has_children'] ? 'true' : 'false' }}">
+                    @include('filament.pages.partials.site-section-row', ['section' => $section])
 
-                @foreach ($section['children'] as $child)
-                    @include('filament.pages.partials.site-section-row', ['section' => $child])
-                @endforeach
+                    @if ($section['children'] !== [])
+                        <div class="artist-page-tree__children" aria-label="Children of {{ $section['navigation_label'] ?: $section['title'] }}">
+                            @foreach ($section['children'] as $child)
+                                @include('filament.pages.partials.site-section-row', ['section' => $child])
+                            @endforeach
+                        </div>
+                    @endif
+                </div>
             @endforeach
         </section>
     </div>
