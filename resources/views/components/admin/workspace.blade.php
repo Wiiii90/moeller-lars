7dc18b36a2bb2510a1a34766f72c38cc06259225
Wilhelm Altemeier <55293156+Wiiii90@users.noreply.github.com>
2026-08-22 14:43:29 +0200

    Add canonical admin workspace primitive

---
+++ resources/views/components/admin/workspace.blade.php
@@ -0,0 +1,21 @@
+@props([
+    'kicker',
+    'title',
+])
+
+<div {{ $attributes->class(['artist-workspace']) }}>
+    <header class="artist-workspace__head">
+        <div>
+            <p class="artist-workspace__kicker">{{ $kicker }}</p>
+            <h2>{{ $title }}</h2>
+        </div>
+
+        @isset($summary)
+            <div class="artist-workspace__summary">
+                {{ $summary }}
+            </div>
+        @endisset
+    </header>
+
+    {{ $slot }}
+</div>
