351031c8908f95c03e4d02dd9e4daa2a01a6b730
Wilhelm Altemeier <55293156+Wiiii90@users.noreply.github.com>
2026-08-22 16:55:44 +0200

    Use canonical site node type in dashboard

--- app/Filament/Widgets/ArtistDashboard.php
+++ app/Filament/Widgets/ArtistDashboard.php
@@ -114,7 +114,7 @@ final class ArtistDashboard extends Widget implements HasActions, HasSchemas
         $editorialGroups = [$artworkStates, $exhibitionStates, $blogStates];
         $drafts = $this->sumStates($editorialGroups, 'draft');
         $hiddenPages = SiteSection::query()
-            ->where('type', '<>', SiteSection::TYPE_NAVIGATION_GROUP)
+            ->where('type', '<>', \App\Domain\Content\SiteNodeType::NavigationNode->value)
             ->where('state', 'hidden')
             ->count();
 
