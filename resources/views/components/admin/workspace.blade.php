ab39b4520eb52b5c022e82050b6002137154e807
Wilhelm Altemeier <55293156+Wiiii90@users.noreply.github.com>
2026-08-22 15:22:46 +0200

    Make workspace primitive canonical

--- resources/views/components/admin/workspace.blade.php
+++ resources/views/components/admin/workspace.blade.php
@@ -3,23 +3,27 @@
     'title',
 ])
 
-<div {{ $attributes->class(['artist-workspace']) }}>
-    <header class="artist-workspace__head">
-        <div>
-            <p class="artist-workspace__kicker">{{ $kicker }}</p>
-            <h2>{{ $title }}</h2>
+<div {{ $attributes->class(['admin-workspace']) }}>
+    <header class="admin-workspace__header">
+        <div class="admin-workspace__heading">
+            <p class="admin-workspace__kicker">{{ $kicker }}</p>
+            <h2 class="admin-workspace__title">{{ $title }}</h2>
         </div>
 
         @isset($summary)
-            <div class="artist-workspace__summary">
+            <div class="admin-workspace__summary">
                 {{ $summary }}
             </div>
         @endisset
 
         @isset($actions)
-            {{ $actions }}
+            <div class="admin-toolbar admin-workspace__actions">
+                {{ $actions }}
+            </div>
         @endisset
     </header>
 
-    {{ $slot }}
+    <div class="admin-workspace__body">
+        {{ $slot }}
+    </div>
 </div>
