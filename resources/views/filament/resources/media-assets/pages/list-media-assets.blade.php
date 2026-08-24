c732de4bcd6d8ff2388859bbd787c6fa158d1fe6
Wilhelm Altemeier <55293156+Wiiii90@users.noreply.github.com>
2026-08-24 07:16:21 +0200

    Keep Media Files controls position stable

--- resources/views/filament/resources/media-assets/pages/list-media-assets.blade.php
+++ resources/views/filament/resources/media-assets/pages/list-media-assets.blade.php
@@ -130,6 +130,7 @@
                         <button
                             class="admin-action is-primary media-workspace__view-trigger"
                             type="button"
+                            style="min-width: 4.75rem;"
                             x-on:click="open = ! open"
                             x-bind:aria-expanded="open.toString()"
                             aria-haspopup="menu"
