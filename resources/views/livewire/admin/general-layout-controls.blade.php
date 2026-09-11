4795a2d4b014e7e6dc8cfb13caafba8dad5f7719
Wilhelm Altemeier <55293156+Wiiii90@users.noreply.github.com>
2026-09-11 22:04:05 +0200

    fix: simplify General layout controls

--- resources/views/livewire/admin/general-layout-controls.blade.php
+++ resources/views/livewire/admin/general-layout-controls.blade.php
@@ -1,9 +1,4 @@
 <section class="general-layout-controls" aria-label="Public page geometry">
-    <div class="general-layout-controls__heading">
-        <span>Page geometry</span>
-        <small>Applied to the public site</small>
-    </div>
-
     <div class="general-layout-controls__fields">
         <label class="general-layout-field">
             <span>Page width</span>
@@ -14,11 +9,9 @@
                     max="1440"
                     step="10"
                     wire:model.live.debounce.350ms="pageWidth"
-                    aria-describedby="general-page-width-help"
                 >
                 <small>px</small>
             </span>
-            <small id="general-page-width-help">Overall public content shell</small>
             @error('pageWidth') <em>{{ $message }}</em> @enderror
         </label>
 
@@ -31,11 +24,9 @@
                     max="180"
                     step="1"
                     wire:model.live.debounce.350ms="contentPadding"
-                    aria-describedby="general-content-padding-help"
                 >
                 <small>px</small>
             </span>
-            <small id="general-content-padding-help">Inset between page edge and artwork</small>
             @error('contentPadding') <em>{{ $message }}</em> @enderror
         </label>
     </div>
