2ae2fa4120c5b09ca07314f239d466b1dd37f71c
Wilhelm Altemeier <55293156+Wiiii90@users.noreply.github.com>
2026-09-12 18:18:47 +0200

    chore: bust admin favicon cache

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -44,7 +44,7 @@ class AdminPanelProvider extends PanelProvider
             ->authPasswordBroker('users')
             ->revealablePasswords(false)
             ->brandName('Admin Area')
-            ->favicon(asset('admin-favicon.svg').'?v=controls-1')
+            ->favicon(asset('admin-favicon.svg').'?v=aperture-1')
             ->homeUrl(fn (): string => route('home'))
             ->breadcrumbs(false)
             ->globalSearch(false)
