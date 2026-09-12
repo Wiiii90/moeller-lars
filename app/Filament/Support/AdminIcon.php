9042f4cfd3ad401d9b1cd38cbfb07ae6f1e0af94
Wilhelm Altemeier <55293156+Wiiii90@users.noreply.github.com>
2026-09-12 20:51:47 +0200

    fix: remove duplicate AdminIcon backing value

--- app/Filament/Support/AdminIcon.php
+++ app/Filament/Support/AdminIcon.php
@@ -18,7 +18,6 @@ enum AdminIcon: string
     case Storage = 'heroicon-o-circle-stack';
     case Preview = 'heroicon-o-viewfinder-circle';
     case PreviewZoom = 'heroicon-o-magnifying-glass';
-    case PreviewZoomIn = 'heroicon-o-magnifying-glass-plus';
     case PreviewZoomOut = 'heroicon-o-magnifying-glass-minus';
     case Commit = 'heroicon-o-check-circle';
     case Artwork = 'heroicon-o-paint-brush';
