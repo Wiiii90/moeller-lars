6bd25ff652d3b0c6f261fc7c1e49bfe7a58c33f7
Wilhelm Altemeier <55293156+Wiiii90@users.noreply.github.com>
2026-09-12 17:58:35 +0200

    chore: distinguish admin document branding

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -43,7 +43,7 @@ class AdminPanelProvider extends PanelProvider
             ->authGuard('web')
             ->authPasswordBroker('users')
             ->revealablePasswords(false)
-            ->brandName('Lars Möller')
+            ->brandName('Admin')
             ->favicon(asset('admin-favicon.svg'))
             ->homeUrl(fn (): string => route('home'))
             ->breadcrumbs(false)
