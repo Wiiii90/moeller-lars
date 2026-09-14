7fc063710a73b7b034aa5ab527e2998f2a689b7b
Wilhelm Altemeier <55293156+Wiiii90@users.noreply.github.com>
2026-09-14 18:27:32 +0200

    ui: align storage usage labels with general appearance

--- resources/views/filament/resources/media-assets/pages/list-media-assets.blade.php
+++ resources/views/filament/resources/media-assets/pages/list-media-assets.blade.php
@@ -1,4 +1,18 @@
 <x-filament-panels::page>
+    @php
+        $usageGroups = array_map(static function (array $group): array {
+            $group['options'] = array_map(static function (array $option): array {
+                if (($option['value'] ?? null) === 'site-identity') {
+                    $option['label'] = 'Site icon';
+                }
+
+                return $option;
+            }, $group['options'] ?? []);
+
+            return $group;
+        }, $usageGroups);
+    @endphp
+
     <x-admin.workspace title="Storage" class="media-workspace admin-storage">
         <x-slot:status>
             <x-admin.status :tone="$capacity['status_tone'] ?? 'neutral'">
