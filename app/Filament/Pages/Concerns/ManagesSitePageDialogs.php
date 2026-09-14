536829f3a5a63677e6b528037c1a2dfcf5aa6f1b
Wilhelm Altemeier <55293156+Wiiii90@users.noreply.github.com>
2026-09-14 20:32:35 +0200

    feat: wire Home presentation controls into Pages

--- app/Filament/Pages/Concerns/ManagesSitePageDialogs.php
+++ app/Filament/Pages/Concerns/ManagesSitePageDialogs.php
@@ -4,6 +4,7 @@ namespace App\Filament\Pages\Concerns;
 
 trait ManagesSitePageDialogs
 {
+    use ManagesHomePagePresentation;
     use ManagesSitePageCreateDialog;
     use ManagesSitePageEditDialog;
 }
