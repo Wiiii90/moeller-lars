d274f2ddd51afc8322d69c528f06ab27d44553bf
Wilhelm Altemeier <55293156+Wiiii90@users.noreply.github.com>
2026-09-14 22:31:44 +0200

    ui: add canonical Skip Home icon

--- app/Filament/Support/AdminIcon.php
+++ app/Filament/Support/AdminIcon.php
@@ -30,6 +30,7 @@ enum AdminIcon: string
     case OpenPublic = 'heroicon-o-arrow-top-right-on-square';
     case OpenEntry = 'heroicon-o-inbox-arrow-down';
     case Redirect = 'heroicon-o-arrow-right';
+    case SkipHome = 'heroicon-o-forward';
     case Publish = 'heroicon-o-eye';
     case Unpublish = 'heroicon-o-eye-slash';
     case Edit = 'heroicon-o-pencil-square';
