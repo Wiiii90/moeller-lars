e94313132e04089b447a97a09750102dd4c76fb0
Wilhelm Altemeier <55293156+Wiiii90@users.noreply.github.com>
2026-09-15 06:10:26 +0200

    chore: remove legacy media route aliases

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,6 @@
 <?php
 
 use App\Filament\Pages\HomePresentation;
-use App\Filament\Resources\MediaAssets\MediaAssetResource;
 use App\Http\Controllers\AdminMediaController;
 use App\Http\Controllers\PublicArtworkController;
 use App\Http\Controllers\PublicContactController;
@@ -37,13 +36,6 @@ Route::middleware(ProtectArtistPreview::class)
     });
 
 Route::get('/admin/home', fn () => redirect()->to(HomePresentation::getUrl(), 301));
-foreach (['media-assets', 'media-files'] as $legacyMediaPath) {
-    Route::get('/admin/'.$legacyMediaPath, fn () => redirect()->to(MediaAssetResource::getUrl('index'), 301));
-    Route::get('/admin/'.$legacyMediaPath.'/{record}/edit', fn (string $record) => redirect()->to(MediaAssetResource::getUrl('edit', ['record' => $record]), 301))
-        ->whereNumber('record');
-    Route::get('/admin/'.$legacyMediaPath.'/{record}', fn (string $record) => redirect()->to(MediaAssetResource::getUrl('view', ['record' => $record]), 301))
-        ->whereNumber('record');
-}
 
 Route::get('/admin/media-preview/original/{mediaAsset}', [AdminMediaController::class, 'original'])
     ->name('admin.media.original');
