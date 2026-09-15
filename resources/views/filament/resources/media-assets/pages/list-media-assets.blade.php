ead05f1203fcaab8e7fa95bc2b03150e6c234729
Wilhelm Altemeier <55293156+Wiiii90@users.noreply.github.com>
2026-09-15 21:46:59 +0200

    fix: remove obsolete storage status component

--- resources/views/filament/resources/media-assets/pages/list-media-assets.blade.php
+++ resources/views/filament/resources/media-assets/pages/list-media-assets.blade.php
@@ -14,12 +14,6 @@
     @endphp
 
     <x-admin.workspace title="Storage" class="media-workspace admin-storage">
-        <x-slot:status>
-            <x-admin.status :tone="$capacity['status_tone'] ?? 'neutral'">
-                {{ $capacity['status_label'] ?? 'Storage unavailable' }}
-            </x-admin.status>
-        </x-slot:status>
-
         @include('filament.resources.media-assets.partials.storage-overview')
         @include('filament.resources.media-assets.partials.storage-library')
     </x-admin.workspace>
