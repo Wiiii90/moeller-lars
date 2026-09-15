f052fadb5a77b5d350e02b143bdcf913e2c9332d
Wilhelm Altemeier <55293156+Wiiii90@users.noreply.github.com>
2026-09-15 19:11:24 +0200

    Remove admin workspace header status

--- resources/views/components/admin/workspace.blade.php
+++ resources/views/components/admin/workspace.blade.php
@@ -2,12 +2,6 @@
     'title',
 ])
 
-@php
-    $automaticStatus = isset($status)
-        ? null
-        : app(\App\Filament\Support\AdminWorkspaceStatus::class)->resolve((string) $title);
-@endphp
-
 <div {{ $attributes->class(['admin-workspace']) }}>
     <header class="admin-workspace__header">
         <div class="admin-workspace__heading">
@@ -19,18 +13,6 @@
                 {{ $summary }}
             </div>
         @endisset
-
-        @if (isset($status))
-            <div class="admin-workspace__status">
-                {{ $status }}
-            </div>
-        @elseif ($automaticStatus !== null)
-            <div class="admin-workspace__status">
-                <x-admin.status :tone="$automaticStatus['tone']">
-                    {{ $automaticStatus['label'] }}
-                </x-admin.status>
-            </div>
-        @endif
     </header>
 
     <div class="admin-workspace__body">
