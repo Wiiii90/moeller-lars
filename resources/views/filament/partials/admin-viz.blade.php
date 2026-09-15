c0cc7f9cdab5dd820ae84020c3eb364702ecae5e
Wilhelm Altemeier <55293156+Wiiii90@users.noreply.github.com>
2026-09-15 19:20:06 +0200

    feat: load admin password tools

--- resources/views/filament/partials/admin-viz.blade.php
+++ resources/views/filament/partials/admin-viz.blade.php
@@ -1,4 +1,5 @@
 @vite('resources/js/admin-viz.js')
+@vite('resources/js/admin-password-tools.js')
 
 @if (request()->is('admin') || request()->is('admin/storage') || request()->is('admin/storage/*'))
     @vite('resources/js/admin-storage-viz.js')
