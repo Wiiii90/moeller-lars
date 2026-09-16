f837b00bbe4aa3bb4ca93e9ed9ec92269b54705f
Wilhelm Altemeier <55293156+Wiiii90@users.noreply.github.com>
2026-09-17 00:56:22 +0200

    fix: allow Pulse Livewire evaluator under CSP

--- app/Http/Middleware/SecurityHeaders.php
+++ app/Http/Middleware/SecurityHeaders.php
@@ -46,7 +46,7 @@ final class SecurityHeaders
     {
         return implode('; ', [
             "default-src 'self'",
-            "script-src 'self' 'unsafe-inline'",
+            "script-src 'self' 'unsafe-inline' 'unsafe-eval'",
             "style-src 'self' 'unsafe-inline' https://fonts.bunny.net",
             "img-src 'self' data:",
             "font-src 'self' data: https://fonts.bunny.net",
