9fd2d5ea1caa663dbe729fcecc1e5ff2fd21a396
Wilhelm Altemeier <55293156+Wiiii90@users.noreply.github.com>
2026-09-17 01:02:21 +0200

    refactor: expose six-cell toolbar contract

--- resources/views/components/admin/controls.blade.php
+++ resources/views/components/admin/controls.blade.php
@@ -1,11 +1,19 @@
 @props([
     'ariaLabel' => null,
+    'metricGrid' => false,
+    'filterCount' => 0,
 ])
+
+@php
+    $normalizedFilterCount = min(max((int) $filterCount, 0), 2);
+@endphp
 
 <div
     {{ $attributes->class([
         'admin-data-controls',
         'admin-data-controls--has-search' => isset($search),
+        'admin-data-controls--six-cell' => $metricGrid,
+        'admin-data-controls--six-cell-filters-'.$normalizedFilterCount => $metricGrid,
     ]) }}
     @if ($ariaLabel) aria-label="{{ $ariaLabel }}" @endif
 >
@@ -17,15 +25,31 @@
         {{ $filters }}
     @endisset
 
-    @isset($reset)
-        {{ $reset }}
-    @endisset
+    @if ($metricGrid)
+        <div class="admin-data-controls__utility">
+            @isset($reset)
+                {{ $reset }}
+            @endisset
 
-    @isset($actions)
-        {{ $actions }}
-    @endisset
+            @isset($actions)
+                {{ $actions }}
+            @endisset
 
-    @isset($selection)
-        {{ $selection }}
-    @endisset
+            @isset($selection)
+                {{ $selection }}
+            @endisset
+        </div>
+    @else
+        @isset($reset)
+            {{ $reset }}
+        @endisset
+
+        @isset($actions)
+            {{ $actions }}
+        @endisset
+
+        @isset($selection)
+            {{ $selection }}
+        @endisset
+    @endif
 </div>
